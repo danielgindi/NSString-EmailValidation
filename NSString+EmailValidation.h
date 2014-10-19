090a73b7ce7cd4f9ded8038693017abf612e5996
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-10-19 15:40:25 +0300

    Initial code commit

---
+++ NSString+EmailValidation.h
@@ -0,0 +1,17 @@
+//
+//  NSString+EmailValidation.h
+//  NSString+EmailValidation
+//
+//  Created by Daniel Cohen Gindi on 3/25/13.
+//  Copyright (c) 2013 Daniel Cohen Gindi. All rights reserved.
+//
+//  https://github.com/danielgindi/NSString-EmailValidation
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (EmailValidation)
+
+- (BOOL)isValidEmailAddress;
+
+@end
